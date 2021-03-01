f07080471db5fdf0a93f9d8610857ad1af1b467f
Gabriel Gatzsche <gabriel.gatzsche@grace.cloud>
2021-03-01 12:40:07 +0100

    Added implementation and test

--- lib/gg_easy_widget_test.dart
+++ lib/gg_easy_widget_test.dart
@@ -1,7 +1,71 @@
-library gg_easy_widget_test;
+// @license
+// Copyright (c) 2019 - 2021 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this repository.
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+/// Retrive helpful information to test a widget
+/// - [T] is the type of the widget under test
+/// - [S] is the type of the widget's state
+class GgEasyWidgetTest<T extends Widget, S extends State> {
+  // ...........................................................................
+  /// The constructor
+  /// - [finder] The finder yielding the widget to be tested
+  /// - [tester] The tester for testing the widget
+  GgEasyWidgetTest(this.finder, this.tester) {
+    expect(finder, findsOneWidget);
+    expect(element, isNotNull);
+  }
+
+  // ...........................................................................
+  /// The used tester.
+  final WidgetTester tester;
+
+  // ...........................................................................
+  /// The used finder
+  final Finder finder;
+
+  // ...........................................................................
+  /// The width of the widget to be tested
+  double get width => renderBox.size.width;
+
+  // ...........................................................................
+  /// The height of the widget to be tested
+  double get height => renderBox.size.height;
+
+  // ...........................................................................
+  /// The render box of the widget
+  RenderBox get renderBox => (element.renderObject as RenderBox);
+
+  // ...........................................................................
+  /// The Element of the tested widget
+  Element get element => tester.firstElement(finder);
+
+  // ...........................................................................
+  /// The absolute frame of the tested widget
+  Rect get absoluteFrame {
+    final offset = renderBox.localToGlobal(Offset.zero);
+    return Rect.fromLTWH(
+        offset.dx, offset.dy, renderBox.size.width, renderBox.size.height);
+  }
+
+  // ...........................................................................
+  /// The widget instance under test
+  T get widget => tester.widget(finder);
+
+  // ...........................................................................
+  /// The state of the widget under test
+  S get state => tester.state(finder);
+
+  // ...........................................................................
+  /// Apply a mouse click or a touch to the widget
+  press() async {
+    final gesture = await tester.press(finder);
+    await tester.pumpAndSettle();
+    await gesture.up();
+    await tester.pumpAndSettle();
+  }
 }
--- test/gg_easy_widget_test_test.dart
+++ test/gg_easy_widget_test_test.dart
@@ -1,12 +1,98 @@
-import 'package:flutter_test/flutter_test.dart';
+// @license
+// Copyright (c) 2019 - 2021 Dr. Gabriel Gatzsche. All Rights Reserved.
+//
+// Use of this source code is governed by terms that can be
+// found in the LICENSE file in the root of this package.
 
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
 import 'package:gg_easy_widget_test/gg_easy_widget_test.dart';
 
-void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+// #############################################################################
+final subwidgetFrame = Rect.fromLTWH(10, 20, 30, 40);
+
+// #############################################################################
+class SubWidget extends StatefulWidget {
+  SubWidget({required Key key}) : super(key: key);
+
+  @override
+  _SubWidgetState createState() => _SubWidgetState();
+}
+
+class _SubWidgetState extends State<SubWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: subwidgetFrame.width,
+      height: subwidgetFrame.height,
+    );
+  }
+}
+
+// #############################################################################
+main() {
+  group('GgEasyWidgetTest', () {
+    // #########################################################################
+
+    testWidgets('should provide information about an sub widget under test',
+        (WidgetTester tester) async {
+      // ......................................
+      // Create a widget containing a subwidget
+      final subWidgetKey = GlobalKey(debugLabel: 'subWidgetKey');
+      final subWidget = SubWidget(key: subWidgetKey);
+
+      final widget = Directionality(
+        textDirection: TextDirection.ltr,
+        child: Container(
+          child: Stack(
+            children: [
+              Positioned(
+                left: subwidgetFrame.left,
+                top: subwidgetFrame.top,
+                child: subWidget,
+              )
+            ],
+          ),
+        ),
+      );
+
+      // ......................
+      // Instantiate the widget
+      await tester.pumpWidget(widget);
+      await tester.pumpAndSettle();
+
+      // .................................
+      // Create a finder for the subwidget
+      final subWidgetFinder = find.byKey(subWidgetKey);
+
+      // .........................................
+      // Create a GgEasyWidgetTest for the subwidget
+      final subGgEasyWidgetTest = GgEasyWidgetTest(subWidgetFinder, tester);
+
+      // ..................................
+      // Width and height should be correct
+      expect(subGgEasyWidgetTest.width, subwidgetFrame.width);
+      expect(subGgEasyWidgetTest.height, subwidgetFrame.height);
+
+      // ..............................
+      // Render box should be delivered
+      expect(subGgEasyWidgetTest.renderBox, isInstanceOf<RenderBox>());
+
+      // ..............................
+      // The element should be delivered
+      expect(subGgEasyWidgetTest.element, isInstanceOf<Element>());
+
+      // ......................................
+      // The absolute frame should be delivered
+      expect(subGgEasyWidgetTest.absoluteFrame, subwidgetFrame);
+
+      // ..............................
+      // The widget should be delivered
+      expect(subGgEasyWidgetTest.widget, subWidget);
+
+      // .............................
+      // The state should be delivered
+      expect(subGgEasyWidgetTest.state, isInstanceOf<_SubWidgetState>());
+    });
   });
 }
